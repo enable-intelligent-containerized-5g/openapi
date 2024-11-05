8f93fbb86968ccc82f30ff0117670bed01773047
edierbravo <edierbra@unicauca.edu.co>
2024-11-05 18:06:25 -0500

    Change MlModelDataResponse model

--- models/model_nwdaf_ml_model_data_response.go
+++ models/model_nwdaf_ml_model_data_response.go
@@ -1,10 +1,5 @@
 package models
 
 type MlModelDataResponse struct {
-	EventId      EventId              `json:"eventId,omitempty" yaml:"eventId" bson:"eventId" mapstructure:"eventId"`
-	Size         int64                `json:"size,omitempty" yaml:"size" bson:"size" mapstructure:"size"`
-	TargetPeriod int                  `json:"targetPeriod,omitempty" yaml:"targetPeriod" bson:"targetPeriod" mapstructure:"targetPeriod"`
-	URI          string               `json:"uri,omitempty" yaml:"uri" bson:"uri" mapstructure:"uri"`
-	Accuracy     NwdafMlModelAccuracy `json:"accuracy,omitempty" yaml:"accuracy" bson:"accuracy" mapstructure:"accuracy"`
-	NfType       NfType               `json:"nfType,omitempty" yaml:"nfType" bson:"nfType" mapstructure:"nfType"`
+	MlModelDataResp      []MlModelData              `json:"mlModelDataResp,omitempty" yaml:"mlModelDataResp" bson:"mlModelDataResp" mapstructure:"mlModelDataResp"`
 }
