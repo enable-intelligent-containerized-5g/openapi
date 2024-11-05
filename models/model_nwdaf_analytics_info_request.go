3e56fd241cc79cf46ca86867317d764d0a430655
Nicolas Chicaiza <jefryn@unicauca.edu.co>
2024-11-05 17:46:58 -0500

    (5nov) Validate: Agregar validacion a solicitud de anlf

--- models/model_nwdaf_analytics_info_request.go
+++ models/model_nwdaf_analytics_info_request.go
@@ -3,10 +3,10 @@ package models
 import "time"
 
 type NwdafAnalyticsInfoRequest struct {
-	StartTime     *time.Time           `json:"startTime" yaml:"startTime" bson:"startTime"`
-	EndTime       *time.Time           `json:"endTime" yaml:"endTime" bson:"endTime"`
-	EventId       EventId              `json:"eventId" yaml:"eventId" bson:"eventId"`
-	Accuracy      NwdafMlModelAccuracy `json:"accuracy" yaml:"accuracy" bson:"accuracy"`
-	NfInstanceIds []string             `json:"nfInstanceIds" yaml:"nfInstanceIds" bson:"nfInstanceIds"`
-	NfTypes       []NfType             `json:"nfTypes" yaml:"nfTypes" bson:"nfTypes"`
+	StartTime     *time.Time           `json:"startTime" yaml:"startTime" bson:"startTime" validate:"required"`
+	EndTime       *time.Time           `json:"endTime" yaml:"endTime" bson:"endTime" validate:"required"`
+	EventId       EventId              `json:"eventId" yaml:"eventId" bson:"eventId" validate:"required"`
+	Accuracy      NwdafMlModelAccuracy `json:"accuracy,omitempty" yaml:"accuracy" bson:"accuracy,omitempty"`
+	NfInstanceIds []string             `json:"nfInstanceIds,omitempty" yaml:"nfInstanceIds" bson:"nfInstanceIds,omitempty"`
+	NfTypes       []NfType             `json:"nfTypes,omitempty" yaml:"nfTypes" bson:"nfTypes,omitempty"`
 }
