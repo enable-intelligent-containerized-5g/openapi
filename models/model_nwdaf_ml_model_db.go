2057e3d8a5a898872702fe54d562731f2c959de8
edierbravo <edierbra@unicauca.edu.co>
2024-11-06 17:41:20 -0500

    Modified NwdafMLModelDB

--- models/model_nwdaf_ml_model_db.go
+++ models/model_nwdaf_ml_model_db.go
@@ -4,8 +4,8 @@ type NwdafMLModelDB string
 
 // Tables for save NwdafMLModelDB
 const (
-	NwdafMLModelDB_ML_MODEL_INFO_TABLE_NAME NwdafMLModelDB = "ml_model_info"
-	NwdafMLModelDB_ACCURACY_TABLE_NAME      NwdafMLModelDB = "accuracy_ids"
-	NwdafMLModelDB_NF_TYPE_TABLE_NAME       NwdafMLModelDB = "nf_types"
-	NwdafMLModelDB_EVENT_ID_TABLE_NAME      NwdafMLModelDB = "event_ids"
+	NwdafMLModelDB_ML_MODEL_KEY NwdafMLModelDB = "MlModel"
+	NwdafMLModelDB_ACCURACY_KEY NwdafMLModelDB = "Accuracy"
+	NwdafMLModelDB_NF_TYPE_KEY  NwdafMLModelDB = "NfType"
+	NwdafMLModelDB_EVENT_ID_KEY NwdafMLModelDB = "Event"
 )
