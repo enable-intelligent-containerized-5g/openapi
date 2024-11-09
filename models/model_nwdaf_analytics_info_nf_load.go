ff58e519a6fb60fd4df68c892b62e0e6fb0de244
edierbravo <edierbra@unicauca.edu.co>
2024-11-09 17:24:01 -0500

    Add models to Stattistics metrics

--- models/model_nwdaf_analytics_info_nf_load.go
+++ models/model_nwdaf_analytics_info_nf_load.go
@@ -1,15 +1,16 @@
 package models
 
-type NwdaAnalyticsInfoNfLoad struct {
-	Accuracy     NwdafMlModelAccuracy `json:"accuracy,omitempty" yaml:"accuracy,omitempty" bson:"accuracy,omitempty" `
-	NfInstanceId string               `json:"nfInstanceId,omitempty" yaml:"nfInstanceId,omitempty" bson:"nfInstanceId,omitempty" `
+type NwdafAnalyticsInfoNfLoad struct {
+	Accuracy     NwdafMlModelAccuracy `json:"accuracy,omitempty" yaml:"accuracy,omitempty" bson:"accuracy,omitempty"`
+	NfInstanceId string               `json:"nfInstanceId,omitempty" yaml:"nfInstanceId,omitempty" bson:"nfInstanceId,omitempty"`
 	NfType       NfType               `json:"nfType,omitempty" yaml:"nfType,omitempty" bson:"nfType,omitempty"`
-	Pod          string               `json:"pod,omitempty" yaml:"pod,omitempty" bson:"pod,omitempty" `
-	CpuUsage     float64              `json:"cpu_usage,omitempty" yaml:"cpu_usage,omitempty" bson:"cpu_usage,omitempty" `
-	MemUsage     float64              `json:"mem_usage,omitempty" yaml:"mem_usage,omitempty" bson:"mem_usage,omitempty" `
-	CpuLimit     float64              `json:"cpu_limit,omitempty" yaml:"cpu_limit,omitempty" bson:"cpu_limit,omitempty" `
-	MemLimit     float64              `json:"mem_limit,omitempty" yaml:"mem_limit,omitempty" bson:"mem_limit,omitempty" `
-	NfLoad       float64              `json:"nf_load,omitempty" yaml:"nf_load,omitempty" bson:"nf_load,omitempty" `
-	NfStatus     NfStatus             `json:"nf_status,omitempty" yaml:"nf_status,omitempty" bson:"nf_status,omitempty" `
-	Confidence   float64              `json:"confidence,omitempty" yaml:"confidence,omitempty" bson:"confidence,omitempty" `
+	Pod          string               `json:"pod,omitempty" yaml:"pod,omitempty" bson:"pod,omitempty"`
+	Container    string               `json:"container,omitempty" yaml:"container,omitempty" bson:"container,omitempty"`
+	CpuUsage     float64              `json:"cpuUsage" yaml:"cpuUsage" bson:"cpuUsage"`
+	MemUsage     float64              `json:"memUsage" yaml:"memUsage" bson:"memUsage"`
+	CpuLimit     float64              `json:"cpuLimit" yaml:"cpuLimit" bson:"cpuLimit"`
+	MemLimit     float64              `json:"memLimit" yaml:"memLimit" bson:"memLimit"`
+	NfLoad       ResourcesNfLoad      `json:"nfLoad" yaml:"nfLoad" bson:"nfLoad"`
+	NfStatus     NfStatus             `json:"nfStatus,omitempty" yaml:"nfStatus,omitempty" bson:"nfStatus,omitempty"`
+	Confidence   float64              `json:"confidence,omitempty" yaml:"confidence,omitempty" bson:"confidence,omitempty"`
 }
